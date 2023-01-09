f3e5825dead900d90d5df8598114739f900757c9
benrit <ritterbwa@gmail.com>
2023-01-09 12:11:54 -0500

    cleanup old code

--- zeissResultLib/__init__.py
+++ zeissResultLib/__init__.py
@@ -51,7 +51,6 @@ class CmmFileHandler:
                         ra.append({"r": float(temp[2])})
         return [{**p, **v, **r} for (p, v, r) in zip(pos, vec, ra)]
 
-
     def loadScanData(self):
 
         if self.setupData.get("importScan", False):
@@ -99,7 +98,6 @@ class CmmFileHandler:
                 self.headerData.update(self.hdrData)
                 print("Done")
 
-
     def loadCHR(self, filename):
         if os.path.isfile(filename) and "chr" in filename:
             file = open(filename, 'r')
@@ -118,7 +116,6 @@ class CmmFileHandler:
             return temp
         else:
             return []
-
 
     def loadFET(self, filename):
         if os.path.isfile(filename) and "fet" in filename:
@@ -142,7 +139,6 @@ class CmmFileHandler:
             return fetData
         else:
             return []
-
     
     def loadHDR(self, filename):
         if os.path.isfile(filename) and "hdr" in filename:
@@ -157,13 +153,11 @@ class CmmFileHandler:
         else:
             return []
 
-
     def loadCMM(self, filename):
         if os.path.isfile(filename):
             with open(filename, 'r') as f:
                 temp = json.load(f)
             return temp
-
 
     def saveCMM(self):
 
@@ -245,96 +239,6 @@ class CmmFileHandler:
                 
         return entry
 
-
-
-def loadDialog(filename):
-    if os.path.isfile(filename) and "dialog" in filename:
-        tree = ET.parse(filename)
-        root = tree.getroot()
-        dialog = root.find("dialog")
-        dialogEntries = {}
-        for dia in dialog.iter():
-            dialogEntries.update({dia.tag:dia.text})
-        del dialogEntries['dialog']
-
-        return dialogEntries
-    else:
-        print("dialog file not found: " + filename)
-        input()
-        exit()
-
-def loadFET(filename):
-    if os.path.isfile(filename) and "fet" in filename:
-        file = open(filename, 'r')
-        fetData = []
-        fileContent = []
-        for line in file:
-            fileContent.append(line.split('\t'))
-        for x,items in enumerate(fileContent[1:-1]):
-            if len(items) > 1:
-                fetData.append(
-                    dict(seq=x, idType=items[4], id=items[2], sigma=items[26], minDev=items[27], maxDev=items[28],
-                         form=items[29], actx=items[8], acty=items[9], actz=items[10], acti=items[11],
-                         actj=items[12], actk=items[13], nomx=items[36], nomy=items[37], nomz=items[38],
-                         nomi=items[39], nomj=items[40], nomk=items[41], act_diameter=items[14],
-                         act_diameter2=items[15], act_a1=items[16], act_a2=items[17], act_angle=items[18], 
-                         act_apexAngle=items[19], nom_diameter=items[42], nom_diameter2=items[43], 
-                         nom_a1=items[44], nom_a2=items[45], nom_angle=items[46], nom_apexAngle=items[47], alignment=items[34]
-                         )
-                )
-        return fetData
-    else:
-        return []
-
-def loadCHR(filename):
-    if os.path.isfile(filename) and "chr" in filename:
-        file = open(filename, 'r')
-        fileContent = []
-        temp = []
-
-        for line in file:
-            fileContent.append(line.split('\t'))
-
-        for x,items in enumerate(fileContent[1:-1]):
-            if len(items) > 1:
-                temp.append(dict(seq=x, idType=items[4], i_id=items[2], act=items[5], nom=items[6], utol=items[7],ltol=items[8], f_id=items[12],group=items[23]))
-        return temp
-    else:
-        return []
-
-
-def loadHDR(filename):
-    if os.path.isfile(filename) and "hdr" in filename:
-        file = open(filename, 'r')
-        hdrData = []
-        fileContent = []
-        for line in file:
-            fileContent.append(line.split('\t'))
-        items = fileContent[1]
-        hdrData = dict(dmeid=items[7], devicegroup=items[8], dmesn=items[9], controllertyp=items[10], dmeswi=items[11], dmeswv=items[12], partnb=items[1])
-        return hdrData
-    else:
-        return None
-
-def updateEntry(entry, data):
-    for new_item in data:
-        found = False
-        for x, item in enumerate(entry):
-            if item.get('i_id') == new_item.get('i_id'):
-                entry[x].update(new_item)
-                found = True
-            if item.get('id') == new_item.get('id'):
-                entry[x].update(new_item)
-                found = True
-                
-        if found == False:
-            entry.append(new_item)
-            
-    return entry
-
-
-def loadScanFiles(filenames):
-    pass
 
 
 
